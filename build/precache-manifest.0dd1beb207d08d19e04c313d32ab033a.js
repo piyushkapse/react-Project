7d590e6413d20edbb87caa58c1c2d2f4b7175d20
piyushkapse <piyushh.kapse@gmail.com>
2021-10-13 17:29:06 +0530

    homepage removed from package.json

---
+++ build/precache-manifest.0dd1beb207d08d19e04c313d32ab033a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e7ad4ae6f8953e8b2fe5f8ecae1906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533273551fc6eb4941dc",
+    "url": "/static/css/main.4b52b7ee.chunk.css"
+  },
+  {
+    "revision": "40848d1207d7dbe0afb3",
+    "url": "/static/js/2.05d6a73f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.05d6a73f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533273551fc6eb4941dc",
+    "url": "/static/js/main.150f317e.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
