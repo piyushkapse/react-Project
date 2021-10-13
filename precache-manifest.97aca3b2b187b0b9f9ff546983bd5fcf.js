66e4f0c7defc2fafd8d5a094463c2e8575a871c2
piyushkapse <piyushh.kapse@gmail.com>
2021-10-13 18:20:41 +0530

    Updates

---
+++ precache-manifest.97aca3b2b187b0b9f9ff546983bd5fcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b074fcc4c9bda619789ff88c3e63e1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bebbedb352470779691",
+    "url": "/static/css/main.4b52b7ee.chunk.css"
+  },
+  {
+    "revision": "f2ca48996d3bea169109",
+    "url": "/static/js/2.e0965986.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e0965986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bebbedb352470779691",
+    "url": "/static/js/main.62f45a0a.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
