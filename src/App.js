71256cc3d043fdbefae49b2293e8eb469cf82ab5
piyushkapse <piyushh.kapse@gmail.com>
2021-10-13 17:50:35 +0530

    deploy changes

--- src/App.js
+++ src/App.js
@@ -10,15 +10,7 @@ import SignUp from './components/pages/SignUp';
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
-        </Switch>
-      </Router>
+      <div><p>Hello world react</p></div>
     </>
   );
 }
